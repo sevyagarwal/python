800b47d0894a65422b2696aa89f484cb27c66794
sevyagarwal <sevyagarwal@gmail.com>
2025-04-08 04:09:50 +0000

    indoor

---
+++ 0- Functions & Variables/indoor.py
@@ -0,0 +1,10 @@
+def main():
+    message = str(input("Enter Phrase:"))
+    print(message.lower())
+
+main()
+
+#1- defined the main function
+#2- message(variabe)- asking user for string(str) input
+#3- printing edited version of message (.lower())
+#5- executing the function we defined in 1-3
